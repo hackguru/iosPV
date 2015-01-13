7179a762ad19e72a4e8fe5f20c9925136374f862
Edward Rezaimehr <erezaimehr@SREZAIMEHR-MBP.home>
2015-01-12 21:39:23 -0800

    Initial Commit

---
+++ PersianVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PersianVideo
+//
+//  Created by Edward Rezaimehr on 1/12/15.
+//  Copyright (c) 2015 Edward Rezaimehr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PersianVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PersianVideo
+//
+//  Created by Edward Rezaimehr on 1/12/15.
+//  Copyright (c) 2015 Edward Rezaimehr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
